4a3a948fbb575b4ad1f9ff382ea23f88e4cff16c
Charles <zhangchaoxu@gmail.com>
2021-10-08 15:40:28 +0800

    fix auth token issue

--- src/views/modules/sys/websocket.vue
+++ src/views/modules/sys/websocket.vue
@@ -1,14 +1,31 @@
 <template>
   <el-card shadow="never" class="aui-card--fill">
     <div class="mod-sys__websocket">
-      <el-form size="small" @submit.native.prevent>
-        <el-form-item>
-          <el-input v-model="websocketAddress" placeholder="WebSocket地址" clearable/>
-        </el-form-item>
-        <el-form-item>
-          <el-button @click="connectWebsocket()" :type="websocket ? 'danger' : 'success'">{{ websocket ? '断开' : '连接'}}</el-button>
-        </el-form-item>
-      </el-form>
+      <el-row>
+        <el-col :span="8">
+          <span>连接数</span>
+        </el-col>
+        <el-col :span="16">
+          <el-form size="small" label-position="left" @submit.native.prevent>
+            <el-form-item label="地址">
+              <el-input v-model="websocketAddress" placeholder="WebSocket地址" clearable/>
+            </el-form-item>
+            <el-form-item class="small-item">
+              <el-button @click="connectWebsocket()" :type="websocket ? 'danger' : 'success'">{{ websocket ? '断开' : '连接'}}</el-button>
+            </el-form-item>
+          </el-form>
+          <el-form size="small" label-position="top" @submit.native.prevent>
+            <el-form-item label="消息">
+              <el-input placeholder="消息" v-model="websocketMessage"
+                        :autosize="{ minRows: 10}"
+                        type="textarea"/>
+            </el-form-item>
+            <el-form-item class="small-item">
+              <el-button @click="connectWebsocket()" type="primary">发送</el-button>
+            </el-form-item>
+          </el-form>
+        </el-col>
+      </el-row>
     </div>
   </el-card>
 </template>
@@ -18,8 +35,9 @@ export default {
   components: { },
   data () {
     return {
+      websocketAddress: process.env.VUE_APP_WS_URL + '/ws/' + this.$store.state.user.id,
       websocket: null,
-      websocketAddress: process.env.VUE_APP_WS_URL + '/ws/' + this.$store.state.user.id
+      websocketMessage: ''
     }
   },
   destroyed () {
@@ -30,37 +48,46 @@ export default {
   methods: {
     // 连接websocket
     connectWebsocket () {
-      if (!this.websocketAddress) {
-        this.$message.error('请配置WebSocket地址')
-        return
-      }
-      if (!('WebSocket' in window)) {
-        this.$message.error('当前浏览器不支持WebSocket')
-        return
-      }
-      console.log(this.websocketAddress)
-      this.websocket = new WebSocket(this.websocketAddress)
-      // 连接错误
-      this.websocket.onerror = function () {
-        console.log('WebSocket连接发生错误   状态码：' + this.websocket.readyState)
-      }
-      // 连接成功
-      this.websocket.onopen = (e) => {
-        console.log(e)
-        // console.log('WebSocket连接成功    状态码：' + this.websocket.readyState)
-      }
-      // 收到消息的回调
-      this.websocket.onmessage = function (event) {
-        // 根据服务器推送的消息做自己的业务处理
-        console.log('服务端返回：' + event.data)
-      }
-      // 连接关闭的回调
-      this.websocket.onclose = function () {
-        console.log('WebSocket连接关闭    状态码：' + this.websocket.readyState)
-      }
-      // 监听窗口关闭事件，当窗口关闭时，主动去关闭websocket连接，防止连接还没断开就关闭窗口，server端会抛异常。
-      window.onbeforeunload = function () {
+      if (this.websocket) {
+        // 已连接,断开连接
         this.websocket.close()
+        this.websocket = null
+      } else {
+        if (!this.websocketAddress) {
+          this.$message.error('请配置WebSocket地址')
+          return
+        }
+        if (!('WebSocket' in window)) {
+          this.$message.error('当前浏览器不支持WebSocket')
+          return
+        }
+        console.log(this.websocketAddress)
+        this.websocket = new WebSocket(this.websocketAddress)
+        // 连接错误
+        this.websocket.onerror = function () {
+          console.log('WebSocket连接发生错误   状态码：' + this.websocket.readyState)
+          this.websocketMessage += 'WebSocket连接发生错误   状态码：' + this.websocket.readyState + '\n'
+        }
+        // 连接成功
+        this.websocket.onopen = (e) => {
+          console.log(e)
+          this.websocketMessage += 'WebSocket连接成功    状态码：' + this.websocket.readyState + '\n'
+        }
+        // 收到消息的回调
+        this.websocket.onmessage = function (event) {
+          // 根据服务器推送的消息做自己的业务处理
+          console.log('服务端返回：' + event.data)
+          this.websocketMessage += '服务端返回：' + event.data + '\n'
+        }
+        // 连接关闭的回调
+        this.websocket.onclose = function () {
+          console.log('WebSocket连接关闭')
+          this.websocketMessage += 'WebSocket连接关闭' + '\n'
+        }
+        // 监听窗口关闭事件，当窗口关闭时，主动去关闭websocket连接，防止连接还没断开就关闭窗口，server端会抛异常。
+        window.onbeforeunload = function () {
+          this.websocket.close()
+        }
       }
     }
   }
